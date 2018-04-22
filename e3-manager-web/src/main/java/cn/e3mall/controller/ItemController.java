1f97834aa63f2283f21b457a3ba2bc16bd67d3cd
HsyAir <1477581881@qq.com>
2018-04-23 02:09:14 +0800

    工程改造之dubbo拆分工程（表现层工程配置文件）

---
+++ e3-manager-web/src/main/java/cn/e3mall/controller/ItemController.java
@@ -0,0 +1,30 @@
+package cn.e3mall.controller;
+
+import cn.e3mall.pojo.TbItem;
+import cn.e3mall.service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author HsY
+ * @create 2018-04-21 23:03
+ * @desc 订单项处理器
+ **/
+@Controller
+public class ItemController {
+
+    @Autowired
+    private ItemService itemService;
+
+    @RequestMapping("/item/{itemId}")
+    @ResponseBody
+    public TbItem getItemById(@PathVariable Long itemId){
+
+        TbItem Item = itemService.getItemById(itemId);
+
+        return Item;
+    }
+}
